7792de6f3ad45ae0cf0fb720c9ea31921cf6525b
Infant Ajay Venus <infantcsajay@outlook.com>
2024-10-30 09:17:15 +0530

    [updateMove] patch: define return types and values

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,4 +4,8 @@ describe("updateMove", () => {
     it('should pass the test', () => {
         expect(updateMove).toBeDefined();
     });
+
+    it("should return co-ordinates", () => {
+        expect(updateMove()).toEqual({ x: 0, y: 0, z: 0, direction: 'N' });
+    });
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export function updateMove() {}
+enum Direction {
+    North = 'N',
+    East = 'E',
+    South = 'S',
+    West = 'W',
+    Up = 'U',
+    Down = 'D'
+};
+interface CoOrdinates {
+    x: number;
+    y: number;
+    z: number;
+    direction: Direction;
+};
+export function updateMove(): CoOrdinates {
+    return { x: 0, y: 0, z: 0, direction: Direction.North }
+}
